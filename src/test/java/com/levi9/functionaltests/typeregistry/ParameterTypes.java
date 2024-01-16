7e5d42ffcb20253dd410640b849b98231b4bd49e
Milos Pujic <m.pujic@levi9.com>
2024-01-16 14:29:57 +0100

    Deleted dummy cukes tests as there is now usage of type registry.

--- src/test/java/com/levi9/functionaltests/typeregistry/ParameterTypes.java
+++ src/test/java/com/levi9/functionaltests/typeregistry/ParameterTypes.java
@@ -8,12 +8,7 @@ import io.cucumber.java.ParameterType;
  * @author Milos Pujic (m.pujic@levi9.com)
  */
 public class ParameterTypes {
-
-	@ParameterType("red|blue|yellow")
-	public Color color(final String color) {
-		return new Color(color);
-	}
-
+	
 	@ParameterType("Single|Twin|Double|Family|Suite")
 	public RoomType roomType(final String roomType) {
 		return RoomType.getEnum(roomType);
